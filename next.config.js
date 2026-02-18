313b7ec5f3c4c0051eae2528cbcbbf8f25dfecc0
kelvex1 <jakeripley73@gmail.com>
2026-02-18 10:24:19 +0800

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-    ],
+    domains: ['images.pexels.com'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
