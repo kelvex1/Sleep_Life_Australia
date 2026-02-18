80c84dfe3d7471ca1c229937ab6ee2219e675c7a
kelvex1 <jakeripley73@gmail.com>
2026-02-18 11:40:26 +0800

    Updated label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
 
-export interface LabelProps
-  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
 
 export const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
-  ({ className = "", ...props }, ref) => {
-    return (
-      <label
-        ref={ref}
-        className={`text-sm font-medium leading-none ${className}`}
-        {...props}
-      />
-    );
-  }
+  ({ className = "", ...props }, ref) => (
+    <label
+      ref={ref}
+      className={`text-sm font-medium leading-none ${className}`}
+      {...props}
+    />
+  )
 );
 Label.displayName = "Label";
