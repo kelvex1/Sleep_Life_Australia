ac8024ce5634ca44468efc3088e65b05e050ef88
kelvex1 <jakeripley73@gmail.com>
2026-02-18 11:33:42 +0800

    Updated alert.tsx

---
+++ components/ui/alert.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+export function Alert({
+  className = "",
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      role="alert"
+      className={`w-full rounded-md border p-4 text-sm ${className}`}
+      {...props}
+    />
+  );
+}
+
+export function AlertTitle({
+  className = "",
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h5 className={`mb-1 font-medium leading-none ${className}`} {...props} />
+  );
+}
+
+export function AlertDescription({
+  className = "",
+  ...props
+}: React.HTMLAttributes<HTMLParagraphElement>) {
+  return <div className={`text-sm ${className}`} {...props} />;
+}
